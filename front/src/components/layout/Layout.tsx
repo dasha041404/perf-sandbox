0fba0a546c5ac5e36a1e3622bd64cf58a8c580f4
kuropatoochka <valya000999@gmail.com>
2026-05-09 16:08:14 +0300

    feat: add header

--- front/src/components/layout/Layout.tsx
+++ front/src/components/layout/Layout.tsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from '../Header'
 import styles from './Layout.module.css'
 
 export function Layout() {
   return (
     <div className={styles.layout}>
-      <header className={styles.header}>
-        <div className={styles.headerPlaceholder}>Header placeholder</div>
-      </header>
+      <Header />
 
       <main className={styles.main}>
         <Outlet />
