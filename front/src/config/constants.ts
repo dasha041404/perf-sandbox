6ae7ef9b21e7de358fd0a7898ffe4dc4a0874022
kuropatoochka <valya000999@gmail.com>
2026-05-10 18:55:28 +0300

    feat: add Sandbox form state and validation

---
+++ front/src/config/constants.ts
@@ -0,0 +1,10 @@
+import type { TemplateEngine } from '../services/backend-types';
+
+export const TEMPLATE_ENGINES: TemplateEngine[] = [
+  'Handlebars',
+  'Mustache',
+  'EJS',
+  'Pug',
+  'Nunjucks',
+  'Liquid',
+];
